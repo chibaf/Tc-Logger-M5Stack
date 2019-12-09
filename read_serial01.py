d5202c7293464fcf110096b907027abca95087f1
chibaf <chibaf@mac.com>
2019-12-10 02:13:51 +0900

    Add files via upload

--- read_serial01.py
+++ read_serial01.py
@@ -12,8 +12,10 @@ while True:
   try:
     line = ser.readline()
     match = regex.findall(line)
+    # "match[4]+"."+match[5]" is the first Tc value, ..., "match[22]+"."+match[23]" is the tenth Tc value.
     f1=match[4]+"."+match[5]+", "+match[6]+"."+match[7]+", "+match[8]+"."+match[9]+", "+match[10]+"."+match[11]+", "+match[12]+"."+match[13]+", "+match[14]+"."+match[15]+", "+match[16]+"."+match[17]+", "+match[18]+"."+match[19]+", "+match[20]+"."+match[21]+", "+match[22]+"."+match[23]
     if(match[3]=="0"):
+    # match[1] is minutes, match[2] is seconds, match[3] is sub seconds.
       print(match[1]+" "+match[2]+" "+match[3]+": "+f1)
     f.write(f1+"\n")
   except KeyboardInterrupt:
